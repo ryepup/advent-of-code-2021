fa12b64836c6d5cfd418f0c49fb3139d770c645a
Ryan Davis <ryepup@gmail.com>
2021-12-01 21:31:28 +0000

    extract a helper for "parse file into ints"
    Uses channels and goroutines probably unnecesarily, but I want to figure out streams

--- day1.go
+++ day1.go
@@ -9,36 +9,53 @@ import (
 	"strconv"
 )
 
+// TODO: is there a better composable "stream" interface for reading / parsing?
+func scan(path string) (<-chan int, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	// TODO: does returning a channel make sense, or is it better to pass one in?
+	results := make(chan int)
+	go func() {
+		defer file.Close()
+		defer close(results)
+
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			line := scanner.Text()
+			// TODO: what's the right thing to do with errors in a channel producer?
+			n, err := strconv.Atoi(line)
+			if err != nil {
+				log.Fatalf("Could not parse %s: %v", line, err)
+			}
+			results <- n
+		}
+
+		if err := scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	return results, nil
+}
+
 // TODO: are there docstrings?
 // cribbed from https://stackoverflow.com/questions/8757389/reading-a-file-line-by-line-in-go
 func Part1(path string) (int, error) {
-	file, err := os.Open(path)
+	depths, err := scan(path)
 	if err != nil {
 		return 0, err
 	}
-	defer file.Close()
-	scanner := bufio.NewScanner(file)
 
-	lastDepth := 0
+	lastDepth := <-depths
 	increases := 0
-	initial := true
 
-	for scanner.Scan() {
-		line := scanner.Text()
-		depth, err := strconv.Atoi(line)
-		if err != nil {
-			return 0, err
-		}
-		if !initial && depth > lastDepth {
+	for depth := range depths {
+		if depth > lastDepth {
 			increases++
 		}
-		initial = false
 		lastDepth = depth
-
-	}
-
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
 	}
 
 	return increases, nil
