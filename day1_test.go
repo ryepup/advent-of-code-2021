775f55c1df1b86bab0580655e78be6c679b058ff
Ryan Davis <ryepup@gmail.com>
2021-12-01 21:06:48 +0000

    get the real day1 part1 data

--- day1_test.go
+++ day1_test.go
@@ -14,3 +14,16 @@ func TestPart1(t *testing.T) {
 		t.Fatalf("Expected %v, got %v", 7, n)
 	}
 }
+
+// TODO: figure out how to use `go run .` to do this, getting a bunch
+// of errors like "adventofcode2021 is not a main package" or
+// found packages adventofcode2021 (day1.go) and main (main.go) in /workspaces/advent-of-code-2021
+func TestPart1RealInput(t *testing.T) {
+	n, err := Part1("day1.part1.txt")
+	if err != nil {
+		t.Fatalf("error: %v", err)
+	}
+	if n != 1292 {
+		t.Fatalf("Expected %v, got %v", 1292, n)
+	}
+}
