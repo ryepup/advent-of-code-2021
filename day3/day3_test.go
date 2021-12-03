e9fad0d9efe5e49472d6b6db5e81072b2c84d324
Ryan Davis <ryepup@gmail.com>
2021-12-03 20:28:28 +0000

    initial part1 implementation

---
+++ day3/day3_test.go
@@ -0,0 +1,20 @@
+package day3_test
+
+import (
+	. "ryepup/advent2021/day3"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPart1(t *testing.T) {
+	n, err := Part1("test.txt")
+	assert.Nil(t, err)
+	assert.Equal(t, 198, n)
+}
+
+func TestPart1_Input(t *testing.T) {
+	n, err := Part1("input.txt")
+	assert.Nil(t, err)
+	assert.Equal(t, 3309596, n)
+}
