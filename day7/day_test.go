84d5cf595c48aa514d65ae90cdfd2de73e5d81b2
Ryan Davis <ryepup@gmail.com>
2021-12-08 01:38:27 +0000

    day7 part 1
    Not too bad, ported a `median` function from wikipedia to find the target

---
+++ day7/day_test.go
@@ -0,0 +1,36 @@
+package day7_test
+
+import (
+	. "ryepup/advent2021/day7"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestPart1(t *testing.T) {
+	testCases := map[string]int{"test.txt": 37, "input.txt": 345197}
+	for path, expected := range testCases {
+		t.Run(path, func(t *testing.T) {
+			if expected == -1 {
+				t.Skip("TODO: provide an expected value")
+			}
+			n, err := Part1(path)
+			require.Nil(t, err)
+			require.Equal(t, expected, n)
+		})
+	}
+}
+
+func TestPart2(t *testing.T) {
+	testCases := map[string]int{"test.txt": -1, "input.txt": -1}
+	for path, expected := range testCases {
+		t.Run(path, func(t *testing.T) {
+			if expected == -1 {
+				t.Skip("TODO: provide an expected value")
+			}
+			n, err := Part2(path)
+			require.Nil(t, err)
+			require.Equal(t, expected, n)
+		})
+	}
+}
